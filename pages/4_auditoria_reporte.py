eb9d18d193dabf8fd35d80994eb3127cf99c23e7
Gonzalo0805 <gonzalo.garciaolivares08@gmail.com>
2026-05-04 20:18:58 +0200

    Update 4_auditoria_reporte.py

--- pages/4_auditoria_reporte.py
+++ pages/4_auditoria_reporte.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.title("Auditoría y Reporte")
+
+st.write("Pipeline:", st.session_state.get("pipeline_ordered"))
+st.write("Roles:", st.session_state.get("roles"))
+st.write("Simulación:", st.session_state.get("sim"))
+
+st.download_button(
+    "Descargar reporte",
+    data=str(st.session_state),
+    file_name="reporte.txt"
+)
